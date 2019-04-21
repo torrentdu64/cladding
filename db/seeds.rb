38d29b7ef449cab1afc286d55c7c701cfa3bcea2
torrent benoit <torrentdu64@gmail.com>
2019-04-22 11:32:05 +1200

    view index scss

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,54 +6,109 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-product = Product.new(category: "Curtain Walls",
-                       brand_name: "hwarrior",
-                        material: "Aluminum frame,glass, accessories",
-                         color: "Customized",
-                         application: "Building Exterior",
-                          thickness: "2mm-4mm",
-                           accessory: "Stainless/Galvanized",
-                            place_of_origin: "Guangdong, China (Mainland)",
-                             model_number: "HW01",
-                              alloy: "aluminum 6063-T5",
-                               surface_treatment: "Anodized",
-                               glass: "Single Glass/Double Glass",
-                               special: "Low-E Glass/Reflected Glass",
-                               usage: "Exterior Facade Curtain Wall",
-                                supply_ability: "20000 Square Meter/Square Meters per Month",
-                                 packaging_detail: "Export wooden case",
-                                  port: "Guangzhou or Shenzhen"
+
+
+product2 = Product.new(category: "Curtain Walls 2",
+                       brand_name: "hwarrior 2",
+                        material: "Aluminum frame,glass, accessories 2",
+                         color: "Customized 2",
+                         application: "Building Exterior 2",
+                          thickness: "1mm-4mm 2",
+                           accessory: "Stainless/Galvanized 2",
+                            place_of_origin: "Guangdong, China (Mainland) 2",
+                             model_number: "HW01 2",
+                              alloy: "aluminum 6063-T5 2",
+                               surface_treatment: "Anodized 2",
+                               glass: "Single Glass/Double Glass 2",
+                               special: "Low-E Glass/Reflected Glass 2",
+                               usage: "Exterior Facade Curtain Wall 2",
+                                supply_ability: "20000 Square Meter/Square Meters per Month 2",
+                                 packaging_detail: "Export wooden case 2",
+                                  port: "Guangzhou or Shenzhen 2"
                                    )
-
-product.save
-
-# product2 = Product.new(type: "Curtain Walls ",
-#                        brand_name: "hwarrior",
-#                         material: "Aluminum frame,glass, accessories",
-#                          color: "Customized",
-#                          application: "Building Exterior",
-#                           thickness: "2mm-4mm",
-#                            accessory: "Stainless/Galvanized",
-#                             place_of_origin: "Guangdong, China (Mainland)",
-#                              model_number: "HW01",
-#                               alloy: "aluminum 6063-T5",
-#                                surface_treatment: "Anodized"
-#                                , glass: "Single Glass/Double Glass"
-#                                , special: "Low-E Glass/Reflected Glass"
-#                                , usage: "Exterior Facade Curtain Wall",
-#                                 supply_ability: "20000 Square Meter/Square Meters per Month",
-#                                  packaging_detail: "Export wooden case",
-#                                   port: "Guangzhou or Shenzhen"
-#                                    )
+product2.save
 
 
 
+product3 = Product.new(category: "Curtain Walls 3",
+                       brand_name: "hwarrior 3",
+                        material: "Aluminum frame,glass, accessories 3",
+                         color: "Customized 3",
+                         application: "Building Exterior 3",
+                          thickness: "1mm-4mm 3",
+                           accessory: "Stainless/Galvanized 3",
+                            place_of_origin: "Guangdong, China (Mainland) 3",
+                             model_number: "HW01 3",
+                              alloy: "aluminum 6063-T5 3",
+                               surface_treatment: "Anodized 3",
+                                glass: "Single Glass/Double Glass 3",
+                                special: "Low-E Glass/Reflected Glass 3",
+                                usage: "Exterior Facade Curtain Wall 3",
+                                supply_ability: "30000 Square Meter/Square Meters per Month 3",
+                                 packaging_detail: "Export wooden case 3",
+                                  port: "Guangzhou or Shenzhen 3"
+                                   )
+product3.save
 
+product4 = Product.new(category: "Curtain Walls 4",
+                       brand_name: "hwarrior 4",
+                        material: "Aluminum frame,glass, accessories 4",
+                         color: "Customized 4",
+                         application: "Building Exterior 4",
+                          thickness: "1mm-4mm 4",
+                           accessory: "Stainless/Galvanized 4",
+                            place_of_origin: "Guangdong, China (Mainland) 4",
+                             model_number: "HW01 4",
+                              alloy: "aluminum 6064-T5 4",
+                               surface_treatment: "Anodized 4",
+                                glass: "Single Glass/Double Glass 4",
+                                special: "Low-E Glass/Reflected Glass 4",
+                                usage: "Exterior Facade Curtain Wall 4",
+                                supply_ability: "40000 Square Meter/Square Meters per Month 4",
+                                 packaging_detail: "Export wooden case 4",
+                                  port: "Guangzhou or Shenzhen 4"
+                                   )
+product4.save
 
+product5 = Product.new(category: "Curtain Walls 5",
+                       brand_name: "hwarrior 5",
+                        material: "Aluminum frame,glass, accessories 5",
+                         color: "Customized 5",
+                         application: "Building Exterior 5",
+                          thickness: "1mm-5mm 5",
+                           accessory: "Stainless/Galvanized 5",
+                            place_of_origin: "Guangdong, China (Mainland) 5",
+                             model_number: "HW01 5",
+                              alloy: "aluminum 6065-T5 5",
+                               surface_treatment: "Anodized 5",
+                                glass: "Single Glass/Double Glass 5",
+                                special: "Low-E Glass/Reflected Glass 5",
+                                usage: "Exterior Facade Curtain Wall 5",
+                                supply_ability: "50000 Square Meter/Square Meters per Month 5",
+                                 packaging_detail: "Export wooden case 5",
+                                  port: "Guangzhou or Shenzhen 5"
+                                   )
+product5.save
 
-
-
-
+product6 = Product.new(category: "Curtain Walls 6",
+                       brand_name: "hwarrior 6",
+                        material: "Aluminum frame,glass, accessories 6",
+                         color: "Customized 6",
+                         application: "Building Exterior 6",
+                          thickness: "1mm-6mm 6",
+                           accessory: "Stainless/Galvanized 6",
+                            place_of_origin: "Guangdong, China (Mainland) 6",
+                             model_number: "HW01 6",
+                              alloy: "aluminum 6066-T6 6",
+                               surface_treatment: "Anodized 6",
+                                glass: "Single Glass/Double Glass 6",
+                                special: "Low-E Glass/Reflected Glass 6",
+                                usage: "Exterior Facade Curtain Wall 6",
+                                supply_ability: "60000 Square Meter/Square Meters per Month 6",
+                                 packaging_detail: "Export wooden case 6",
+                                  port: "Guangzhou or Shenzhen 6"
+                                   )
+product6.save
 
 
 
