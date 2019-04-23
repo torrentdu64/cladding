896ae7652ce70262c89d6ca6055a94817bf21ed5
torrent benoit <torrentdu64@gmail.com>
2019-04-23 16:37:47 +1200

    in contact model after create send email

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,10 @@
 class Contact < ApplicationRecord
 
   validates :email, :description, presence: true , format: { with: /\A.*@.*\.com\z/ }
+  after_create :send_info_email
+
+  def send_info_email
+    ContactMailer.info(self).deliver_now
+  end
 
 end
